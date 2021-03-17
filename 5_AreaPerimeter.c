456cdb49ace074c0e0052781951a767fd04dc616
shaukat-ishtiaq <you@example.com>
2021-03-17 21:01:42 +0530

    Uploading 5_AreaPerimeter to the repository

--- 5_AreaPerimeter.c
+++ 5_AreaPerimeter.c
@@ -9,7 +9,7 @@ int main()
 	printf("Perimeter of circle: %f\n", (2*3.14*radius));
 	printf("Area of square: %f\n", (side*side));
 	printf("Perimeter of square: %f\n", (4*side));
-	printf("Area of rectangle: %f\n", (2*(length+breadth)));
+	printf("Perimeter of rectangle: %f\n", (2*(length+breadth)));
 	printf("Area of rectangle: %f\n", (length*breadth));
 	return 0;
 }
