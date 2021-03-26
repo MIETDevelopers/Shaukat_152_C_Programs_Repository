39eb42115d500d4a202952ee6096faeeb5bf72e1
shaukat-ishtiaq <you@example.com>
2021-03-26 14:03:53 +0530

    Uploading Hello World program for assignment

--- 3_Hello.c
+++ 3_Hello.c
@@ -5,7 +5,7 @@
 
 //Start of the main body function
 int main() {		
-	printf("HELLO WORLD");	//Print "Hello World" on the screen
+	printf("HELLO WORLD!");	//Print "Hello World" on the screen
 	return 0;	//return statement
 }
 //End of the main body function
