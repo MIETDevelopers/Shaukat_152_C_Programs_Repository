7d9a12f56c56ada0d7024d387df819415c718642
shaukat-ishtiaq <you@example.com>
2021-03-27 22:56:32 +0530

    A1 - Q38 git status

---
+++ 45_readCsvFile.c
@@ -0,0 +1,24 @@
+//Purpose: Read student data from a CSV file.
+//Author: Shaukat Ishtiaq
+//Date: 03-27-2021
+#include <stdio.h>	//Preprocessor directive to include standard input output header file
+//Start of the main body function
+int main() {
+    FILE *pointer = fopen("student.csv","r");
+
+    char data;
+    int rollNo;
+    float attendancePercentage;
+
+
+
+    //fprintf(pointer,"Name,RollNo,Percentage\n");
+    //fprintf(pointer,"%s,%d,%f\n",name,rollNo,attendancePercentage);
+
+    do {
+        data = fgetc(pointer);
+        printf("%c",data);
+    }while(data != EOF);
+    fclose(pointer);
+    return 0;   //return statement
+}
